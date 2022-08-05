9850e2f4c4e4cf1e0f6fee7a547b21bf58c8a547
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-08-05 10:23:00 +0900

    message from webSocket

--- src/server.js
+++ src/server.js
@@ -18,4 +18,16 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.send("hello!!!");
+});
+
 server.listen(3000, handleListen);
