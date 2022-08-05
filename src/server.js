c1cf65af2dbc32dd74d2bb16a0ee61b83a286577
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-08-05 10:46:15 +0900

    chat & nickname

--- src/server.js
+++ src/server.js
@@ -1,33 +1,42 @@
-import express from "express";
 import http from "http";
 import WebSocket from "ws";
+import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => {
-  console.log(`Listening on port http://localhost:3000`);
-};
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
 }
+
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (message) => {
-    console.log(message);
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString());
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
-  socket.send("hello!!!");
 });
 
 server.listen(3000, handleListen);
